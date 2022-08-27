b1aab2779026b73990ce5be5d8b1f7f199ec34c0
Vedat Biner <vbiner@gmail.com>
2022-08-27 13:27:01 +0300

    SignIn Google

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -31,22 +31,28 @@ class Auth {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    if(googleUser != null) {
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    // Once signed in, return the UserCredential
-    UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
-    return userCredential.user;
+      // Once signed in, return the UserCredential
+      UserCredential userCredential = await _firebaseAuth.signInWithCredential(credential);
+      return userCredential.user;
+    } else {
+      return null;
+    }
+
   }
 
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
+    await GoogleSignIn().signOut();
   }
 
   Stream<User?> authStatus() {
